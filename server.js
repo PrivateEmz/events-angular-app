c09838bdc751abedc8e8247d6f900e13248f7b9b
Bbaale15 <bbaale.emmy@yahoo.com>
2019-12-16 16:22:33 +0300

    Ugraded angular to 8

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
  
 const app = express();
  
-// Serve only the static files form the angularapp directory
+// Serve only the static files from buid directory
 app.use(express.static(__dirname + '/eventsapp'));
  
 app.get('/*', function(req,res) {
